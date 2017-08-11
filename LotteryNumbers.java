5e6df903e7547e94529b9f6068ce1fa732c1071e
U-FHB\kbarroga <kbarroga@OSDLOCKBOX002A.FHB.NET>
2017-08-11 13:46:30 -1000

    Add main method to test

--- LotteryNumbers.java
+++ LotteryNumbers.java
@@ -3,12 +3,14 @@ import java.util.Random;
 
 public class LotteryNumbers {
     private ArrayList<Integer> numbers;
+    private Random random;
 
     public LotteryNumbers() {
         // We'll format a list for the numbers
         this.numbers = new ArrayList<Integer>();
         // Draw numbers as LotteryNumbers is created
         this.drawNumbers();
+        this.random = new Random();
     }
 
     public ArrayList<Integer> numbers() {
@@ -17,11 +19,17 @@ public class LotteryNumbers {
 
     public void drawNumbers() {
         // Write the number drawing here using the method containsNumber()
+        int rand = this.random.nextInt(38) + 1
+        for (int i = 0; i <= 6; i++){
+            if(!this.containsNumber(rand)) {
+                numbers.add(rand)
+            }
+        }
     }
 
     public boolean containsNumber(int number) {
         // Test here if the number is already among the drawn numbers
-		return true;
+		return numbers.contains(number);
     }
 
     public static void main(String[] args) {
