4b9e2195fb649548870d3a91c9aa9adcca36285a
U-FHB\kbarroga <kbarroga@OSDLOCKBOX002A.FHB.NET>
2017-08-14 10:31:00 -1000

    Add PersonWithPhoneNumber class

---
+++ PersonWithPhoneNumber.java
@@ -0,0 +1,40 @@
+
+import java.util.Calendar;
+
+public class Person {
+
+    private String name;
+    private String number
+
+    public Person(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void changeNumber(String newNumber) {
+        this.number = newNumber;
+    }
+
+    public String toString() {
+        return this.name + ", number " + this.number;
+    }
+
+    public static void main(String[] args) {
+        Person pekka = new Person("Pekka Mikkola", "040-123123");
+
+        System.out.println(pekka.getName());
+        System.out.println(pekka.getNumber());
+
+        System.out.println(pekka);
+        pekka.changeNumber("050-333444");
+        System.out.println(pekka);
+    }
+}
